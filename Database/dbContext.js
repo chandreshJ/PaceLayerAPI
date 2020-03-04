17045c3888b01608077dee8df4b37ba8ae1a2318
chandreshJ <60393918+chandreshJ@users.noreply.github.com>
2020-03-04 21:45:01 +0530

    commit v3

--- Database/dbContext.js
+++ Database/dbContext.js
@@ -1,6 +1,6 @@
 var Request = require('tedious').Request;
 var connection = require('../Database/connect');
-
+var TYPES = require('tedious').TYPES;
 var utility = require('../Database/utility/utility');
 
 function spGetExecute(qry, callback) {
@@ -24,33 +24,35 @@ function spGetExecute(qry, callback) {
 
     connection.callProcedure(request);
 }
-
 function spPostExecute(qry, params, callback) {
+    console.log("cnt params "+params.length);
+    if(params.length>0){
     var newdata = [];
-
-    request = new Request(qry, function (err, rowCount) {
-        utility.sendDbResponse(err, rowCount, newdata, callback);
-    });
-
-    params.forEach(param => {
-
-        request.addParameter(param.name, param.type, param.val);
-
-    });
-
-    request.on('row', function (columns) {
-        utility.buildRow(columns, newdata);
-    });
-
-    connection.callProcedure(request);
+      request = new Request(qry, function (err, rowCount) {
+          utility.sendDbResponse(err, rowCount, newdata, callback);
+      });
+      request.addParameter('mode',TYPES.VarChar, 'ADD');
+      params.forEach(param => {
+  //        console.log(param.val);
+          request.addParameter(param.name, param.type, param.val);
+      });
+  console.log('sp Post Execute');
+      
+      request.on('row', function (columns) {
+          utility.buildRow(columns, newdata);
+      });
+      connection.callProcedure(request);
+  }
+  else
+      return null;
 }
-
+  
 function queryGetExecute(qry, params, isMultiSet, callback) {
     var data = [];
     var dataset = [];
     var resultset = 0;
 
-    console.log('hi')
+    console.log('log: queryGetExecute')
     request = new Request(qry, function (err, rowCount) {
         utility.sendDbResponse(err, rowCount, dataset, callback);
 
@@ -73,7 +75,7 @@ function queryGetExecute(qry, params, isMultiSet, callback) {
         }
     });
 
-    connection.execSql(request);
+    connection.callProcedure(request);
 }
 
 function queryExecute(qry, params, isMultiSet, callback) {
--- routes/masterApi.js
+++ routes/masterApi.js
@@ -4,35 +4,97 @@ const dbContext = require('../Database/dbContext');
 var response = require('../shared/response');
 //var response = require('../../shared/response');
 var TYPES = require('tedious').TYPES;
- 
+var bodyParser = require('body-parser')
+var customParser = bodyParser.json({type: function(req) {
+  req.headers['content-type'] === "text/html"
+}})
+router.post('/AddPort', customParser, function(req, res){
+	var newTask = {
+		//idTasks : shortid.generate(),
+		Name : 'Content-type: ' + req.get('Content-type'),
+		Description : 'JSON body: ' + JSON.stringify(req.body),
+		CreatedDateTime : new Date()
+	}
+  var parameters = [];
+  for (var key in req.body) {
+      parameters.push({ name: key, type: TYPES.Int, val: req.body[key] });
+  }
+	//queryHandler.queryRecordsWithParam('insert into Tasks set ?', newTask, function(err, rows){
+    if(parameters.length>0){
+    dbContext.post("CRUD_AppSupportOption",parameters, function (error, data) {
+		if(error) {
+      console.log('info: ', '-------------------- ERROR: ' + error);
+     return res.status(400).json({
+        status: 'error',
+        error: error.message,
+      });
+    }
+    return	 res.status(200).json({
+      status: 'succes',
+      data: 'Record added successfully.',
+    });
+  });
+}
+else res.sendStatus(300).json('No param');
+});
 
-// router.get('/', function(req, res) {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-//   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+//  var _appBrpocess = {
+//   portfolioID : 0,
+//   applicationID : 0,
+//   BprocessID :0,
+//   SupportOptionID : 0,
+//   };
+// function AppBrpocess () {/*from  ww  w  .j a v  a 2  s. co  m*/
 
-//   res.send('cors problem fixed:)');
-// });
-
-
+// }
 /* GET portfolios listing. */
 router.get('/portfolios/', function(req, res, next) {
   dbContext.get("getportfolio", function (error, data) {
+    //console.log(error);
+    if(error)
+    return res.status(400).json({
+      status: 'error',
+      error: error.message,
+    });
+    else
+    return	 res.status(200).json({
+      status: 'succes',
+      data: (response(data, error))
+    });
+  });
+  
+});
+ /* GET ApplLayerState listing. */
+ router.get('/ApplLayerState/', function(req, res, next) {
+   dbContext.get("getApplLayerState", function (error, data) {
 
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-//   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-//   res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-return res.json(response(data, error));
- });
- });
-/* GET applications listing. */
-router.get('/applications/:ID', function(req, res, next) {
-  dbContext.getQuery("getapplications", function (error, data) {
-   return res.json(response(data, error));
- });
- });
+    return res.json(response(data, error));
+  });
+  });
+  /* GET applications listing. */
+  router.get('/applications/:ID', function(req, res, next) {
+    if (req.params.ID) {
+      var parameters = [];
+
+      parameters.push({ name: 'PortfolioId', type: TYPES.Int, val: req.params.ID });
+
+      dbContext.getQuery("getapplications",parameters,true, function (error, data) {
+        console.log(data);
+     return res.json(response(data, error));
+   });
+  }});
+  /* GET ApplLayer State listing. */
+  router.get('/ApplLayerStateID/:ID', function(req, res, next) {
+    if (req.params.ID) {
+      var parameters = [];
+
+      parameters.push({ name: 'ID', type: TYPES.Int, val: req.params.ID });
+
+      dbContext.getQuery("getApplLayerState",parameters,true, function (error, data) {
+        console.log(data);
+     return res.json(response(data, error));
+   });
+  }});
 /* GET Process listing. */
 router.get('/BProcess/', function(req, res, next) {
   dbContext.get("getBProcess", function (error, data) {
@@ -44,5 +106,5 @@ router.get('/BProcess/', function(req, res, next) {
     return res.json(response(data, error));
   });
   });
-   
+
 module.exports = router;
